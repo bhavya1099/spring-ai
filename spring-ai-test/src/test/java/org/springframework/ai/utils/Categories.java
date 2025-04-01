4c326fbda21913f0e7e098269021ef043da9e8c0
bhavya1099 <kinshu.1019@gmail.com>
2025-04-01 11:26:22 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ spring-ai-test/src/test/java/org/springframework/ai/utils/Categories.java
@@ -0,0 +1,25 @@
+package org.springframework.ai.utils;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface play {
+
+	}
+
+}
