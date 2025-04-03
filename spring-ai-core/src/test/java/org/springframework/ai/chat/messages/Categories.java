a8fc2d0b4197c10ab41ff95da9bdbee3e0a0aa3a
bhavya1099 <kinshu.1019@gmail.com>
2025-04-03 11:44:23 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ spring-ai-core/src/test/java/org/springframework/ai/chat/messages/Categories.java
@@ -0,0 +1,41 @@
+package org.springframework.ai.chat.messages;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface getText {
+
+	}
+
+	interface getMetadata {
+
+	}
+
+	interface getMessageType {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+}
